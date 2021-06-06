edc99433c2d2a6f0901c30cc5067a9816747594c
jlbuild <juliabuildbot@gmail.com>
2021-06-06 18:29:57 +0000

    WCS_jll build 7.6.0+0

--- src/wrappers/x86_64-apple-darwin.jl
+++ src/wrappers/x86_64-apple-darwin.jl
@@ -2,12 +2,12 @@
 export libwcs
 
 JLLWrappers.@generate_wrapper_header("WCS")
-JLLWrappers.@declare_library_product(libwcs, "@rpath/libwcs.7.5.dylib")
+JLLWrappers.@declare_library_product(libwcs, "@rpath/libwcs.7.6.dylib")
 function __init__()
     JLLWrappers.@generate_init_header()
     JLLWrappers.@init_library_product(
         libwcs,
-        "lib/libwcs.7.5.dylib",
+        "lib/libwcs.7.6.dylib",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
